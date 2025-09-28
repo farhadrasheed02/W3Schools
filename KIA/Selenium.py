c6623094c5e4feebcc59e4d42725cb2b3f93a6be
Farhad Rashid Reshi <42071670+farhadrasheed02@users.noreply.github.com>
2025-09-28 18:19:22 +0530

    FR
    Commit by FR

--- KIA/Selenium.py
+++ KIA/Selenium.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("https://www.kia.gov.kw/")
+driver.maximize_window()
+tittle = driver.title
 
-# Your Selenium code here
-driver.quit()
+about = driver.find_element(By.LINK_TEXT, "About")
+driver.wait(10)
+about.click()
+
+# print(tittle)
