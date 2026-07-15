e7f8b6f03a57b8acac340984fb716d29b57a393c
Farhad Rashid Reshi <42071670+farhadrasheed02@users.noreply.github.com>
2026-07-16 01:18:05 +0530

    16

--- Python_100_Days/Basic_Python.py
+++ Python_100_Days/Basic_Python.py
@@ -1,8 +1,9 @@
 import pandas
 import calendar
 # print("Farhad Rashid Reshi")
-# print("Pandas Version",pandas.__version__)
+# print ("Pandas Version",pandas.__version__)
 # print(calendar.calendar(2026))
 # print("Hello, World!")
 # print("This is my first python program \nand I am working on this")
-print("Farhad",sep="Love",end=" Rashid")
+print("Farhad",sep="~",end=" Rashid \n")  #Seperator will not work as we have only one argument.
+print("Farhad",7, sep="Loves", end="Rashid") # here seperator will work as we have more than one argument.
