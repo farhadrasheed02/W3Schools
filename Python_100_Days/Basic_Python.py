bebaec16ef309df89ba9361f0f9c3c7d0de90ef3
Farhad Rashid Reshi <42071670+farhadrasheed02@users.noreply.github.com>
2026-07-15 21:21:06 +0530

    15 July 2026

--- Python_100_Days/Basic_Python.py
+++ Python_100_Days/Basic_Python.py
@@ -1,1 +1,8 @@
-print("Farhad Rashid Reshi")
+import pandas
+import calendar
+# print("Farhad Rashid Reshi")
+# print("Pandas Version",pandas.__version__)
+# print(calendar.calendar(2026))
+# print("Hello, World!")
+# print("This is my first python program \nand I am working on this")
+print("Farhad",sep="Love",end=" Rashid")
