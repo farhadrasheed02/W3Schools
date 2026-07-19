a4b83b5537c1d046c1419c193caf52be991720dd
Farhad Rashid Reshi <42071670+farhadrasheed02@users.noreply.github.com>
2026-07-20 01:12:05 +0530

    Commit by Farhad

--- Python_100_Days/Break_Continue.py
+++ Python_100_Days/Break_Continue.py
@@ -1,9 +1,11 @@
 from Utilities import Logging_Report
-
 for i in range(10):
     print("Multiplication Table of 5: ", 5 * i)
     Logging_Report.logger.info(f"Multiplication Table of 5: {5 * i}")
     if i == 5:
+        Logging_Report.logger.info(f"Continuing the loop at i: {i}")
+        continue
+    elif i == 8:
         Logging_Report.logger.info(f"Breaking the loop at i: {i}")
         break
     else:
