af6017af1af703134db070b8ed8fa512a88e9f3c
Farhad Rashid Reshi <42071670+farhadrasheed02@users.noreply.github.com>
2026-07-23 00:01:47 +0530

    Commit

---
+++ Python_100_Days/List_Working.py
@@ -0,0 +1,6 @@
+marks = [85, 90, 78, 92, 88,"Farhad",True]  # we can add different data types in a list this is called heterogeneous list
+print("Marks:", marks)
+# print("Highest mark:", max(marks))
+print(marks[3])
+print(marks[7])  #IndexError: list index out of range
+
