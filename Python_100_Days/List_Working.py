ba802f150777940dd9cd50d1ba453ce37b681772
Farhad Rashid Reshi <42071670+farhadrasheed02@users.noreply.github.com>
2026-07-23 23:57:56 +0530

    Commit

--- Python_100_Days/List_Working.py
+++ Python_100_Days/List_Working.py
@@ -2,5 +2,14 @@ marks = [85, 90, 78, 92, 88,"Farhad",True]  # we can add different data types in
 print("Marks:", marks)
 # print("Highest mark:", max(marks))
 print(marks[3])
-print(marks[7])  #IndexError: list index out of range
+# print(marks[7])  #IndexError: list index out of range
+if "Farhad" in marks:
+    print("Farhad is present in the list")
+
+
+
+import array as arr
+arr_1 = arr.array('i',(1, 2, 3, 4, 5))  # creating an array of integers
+print(type(arr_1.))  # printing the type of the array
+print("Array:", arr_1)
 
