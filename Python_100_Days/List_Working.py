785b865f07ed683012fbf205562d33c11658ecf5
Farhad Rashid Reshi <42071670+farhadrasheed02@users.noreply.github.com>
2026-07-24 00:09:00 +0530

    Commit

--- Python_100_Days/List_Working.py
+++ Python_100_Days/List_Working.py
@@ -5,11 +5,13 @@ print(marks[3])
 # print(marks[7])  #IndexError: list index out of range
 if "Farhad" in marks:
     print("Farhad is present in the list")
+marks.append(95)  # adding an element to the end of the list
+print("Marks after appending:", marks)
 
-
-
+# :: -1 means start:stop:step
+print("Marks in reverse order:", marks[::-1])  # printing the list in reverse order
 import array as arr
 arr_1 = arr.array('i',(1, 2, 3, 4, 5))  # creating an array of integers
-print(type(arr_1.))  # printing the type of the array
+print(type(arr_1))  # printing the type of the array
 print("Array:", arr_1)
 
