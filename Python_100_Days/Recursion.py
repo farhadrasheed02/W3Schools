305cab683abd4aadc3f14d49254d963a23cac26a
Farhad Rashid Reshi <42071670+farhadrasheed02@users.noreply.github.com>
2026-07-26 19:17:14 +0530

    Commit

--- Python_100_Days/Recursion.py
+++ Python_100_Days/Recursion.py
@@ -1,11 +1,15 @@
 from Utilities.Input_file import user_input as user_input_value
-from Utilities.Functions import factorial as fac
+from Utilities import Functions as fun
 
 
 us_input = user_input_value() # Taking User Input from the Input_file
-#Factorial Function getting implemented..
-fact_value = fac(us_input) #Passing Factorial function the input from the user
+#Factorial Function getting implemented.
+fact_value = fun.factorial(us_input) #Passing Factorial function the input from the user
 print(f"Factorial  of {us_input} is :",fact_value)
-print(fac.__doc__) #doc string getting used here. buddy
+print(fun.factorial.__doc__) #doc string getting used here. buddy
 
-#Fibonacci Series
+#Fibonacci Series getting implemented...
+Fib_Series = []
+for i in range(us_input):
+    Fib_Series.append(fun.Fibonacci(i))
+print(f"Fibonacci Sequence for {us_input} is :",Fib_Series)
--- Utilities/Functions.py
+++ Utilities/Functions.py
@@ -27,10 +27,12 @@ def factorial(n):
         return n* factorial(n-1)
 
 def Fibonacci(n):
-    if n==0:
+    if n ==1:
+        return n
+    elif n==0:
         return 0
-    elif n==1:
-        return 1
-    else:
-        return Fibonacci(n-1) + Fibonacci(n-2)
-    
+    return Fibonacci(n-1) + Fibonacci(n-2)
+
+
+
+
