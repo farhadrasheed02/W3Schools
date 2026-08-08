2b3a98f72fcd8410a9017cb6a6c385568853bc2e
Farhad Rashid Reshi <42071670+farhadrasheed02@users.noreply.github.com>
2026-08-08 23:39:25 +0530

    Commit by Farhad

--- Python_100_Days/Files/File_Operations.py
+++ Python_100_Days/Files/File_Operations.py
@@ -1,1 +1,11 @@
-f = open("test.txt", "w")
+f = open("test.txt", "a")
+f.write("This is a test file. \n")
+f.close()
+f1 = open("test.txt", "r")
+content = f1.read()
+print(content)
+f.close()
+
+
+
+
