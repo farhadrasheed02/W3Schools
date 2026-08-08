af1d36321f3e98be18dbd2edc6bbc0972308dfba
Farhad Rashid Reshi <42071670+farhadrasheed02@users.noreply.github.com>
2026-08-09 00:45:26 +0530

    Commit

--- Python_100_Days/Files/File_Operations.py
+++ Python_100_Days/Files/File_Operations.py
@@ -1,11 +1,15 @@
-f = open("test.txt", "a")
-f.write("This is a test file. \n")
-f.close()
 f1 = open("test.txt", "r")
-content = f1.read()
-print(content)
-f.close()
+while True:
+    line =f1.readline()
+    if not line:
+        break
+    print(line)
 
+# f.write("This is a test file. \n")
+#
+# with open("test.txt", "r") as f:
+#     content = f.read()
+#     print(content)
 
 
 
